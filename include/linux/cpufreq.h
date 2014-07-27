15d64f3712bc1797b0dc21b25a0704c8ea053bc9
RoGod <rogersalvarez.ra@gmail.com>
2014-07-27 07:06:58 -0430

    Governador Lulzactiveq

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -384,7 +384,9 @@ extern struct cpufreq_governor cpufreq_gov_wheatley;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
-
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVEQ)
+extern struct cpufreq_governor cpufreq_gov_lulzactiveq;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lulzactiveq)
 #endif
 
 /*********************************************************************
