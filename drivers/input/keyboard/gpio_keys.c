bbc20bc52edd030fce164fd63112318fd1675e3a
RoGod <rogersalvarez.ra@gmail.com>
2014-07-28 21:20:31 -0430

    Touch Wake 1.1

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -26,9 +26,10 @@
 #include <linux/workqueue.h>
 #include <linux/gpio.h>
 #include <linux/irqdesc.h>
-#ifdef CONFIG_FAST_BOOT
 #include <linux/fake_shut_down.h>
 #include <linux/wakelock.h>
+#ifdef CONFIG_TOUCH_WAKE
+#include <linux/touch_wake.h>
 #endif
 
 extern struct class *sec_class;
@@ -955,6 +956,10 @@ static int __devinit gpio_keys_probe(struct platform_device *pdev)
 			wakeup = 1;
 
 		input_set_capability(input, type, button->code);
+		
+		#ifdef CONFIG_TOUCH_WAKE
+ 			  set_powerkeydev(input);
+ 		#endif
 	}
 
 	error = sysfs_create_group(&pdev->dev.kobj, &gpio_keys_attr_group);
