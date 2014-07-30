fca95db6844dd2674ab7b16a3f3cb793780b84c0
RoGod <rogersalvarez.ra@gmail.com>
2014-07-30 19:21:23 -0430

    Gobernador Darkness

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -393,6 +393,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR    (&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR    (&cpufreq_gov_darkness)
 #endif
 
 /*********************************************************************
