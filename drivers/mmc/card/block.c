abe5d3080823874dc48ff723ffc2e0523ca83e24
RoGod <rogersalvarez.ra@gmail.com>
2014-08-01 19:27:54 -0430

    Soporte TriangleAway

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -2530,7 +2530,10 @@ static int mmc_blk_probe(struct mmc_card *card)
 	printk(KERN_INFO "%s: %s %s %s %s\n",
 		md->disk->disk_name, mmc_card_id(card), mmc_card_name(card),
 		cap_str, md->read_only ? "(ro)" : "");
-
+		
+    if (mmc_blk_alloc_parts(card, md))
+ 		goto out;
+ 
 	mmc_set_drvdata(card, md);
 	mmc_fixup_device(card, blk_fixups);
 
